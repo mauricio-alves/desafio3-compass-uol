a73fa9ef5550cf39815b8eac1cb602d823dea2ee
mauricioalves <mauricioalvesdev@gmail.com>
2025-07-07 23:42:16 -0300

    adjustments done

--- frontend/src/components/common/ShowMore/ShowMore.tsx
+++ frontend/src/components/common/ShowMore/ShowMore.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export function ShowMore() {
   return (
     <div className="flex justify-center mt-8 gap-6">
-      <button className="border border-[var(--color-yellow)] text-[var(--color-yellow)] py-3 px-20 rounded hover:bg-[var(--color-yellow)] hover:text-white transition cursor-pointer font-poppins">
-        <Link to="/category/all">Show More</Link>
-      </button>
+      <Link to="/category/all" className="border border-[var(--color-yellow)] text-[var(--color-yellow)] py-3 px-20 rounded hover:bg-[var(--color-yellow)] hover:text-white transition cursor-pointer font-poppins">
+        Show More
+      </Link>
     </div>
   );
 }
